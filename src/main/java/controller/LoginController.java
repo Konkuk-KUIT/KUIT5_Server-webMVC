0e29e69031ef6adfc8a1b088bbc29a483177ed8f
Jay Park <pjh6183@naver.com>
2025-04-08 19:30:22 +0900

    feat: 1단계 요구사항 4 기능구현

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import core.db.MemoryUserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jwp.model.User;
+
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+
+        User findUser = MemoryUserRepository.getInstance().findUserById(userId);
+
+        if (findUser == null || !findUser.matchPassword(password)) {
+            resp.sendRedirect("/user/login_failed.jsp");
+            return;
+        }
+
+        HttpSession session = req.getSession();
+        session.setAttribute("user", findUser);
+
+        resp.sendRedirect("/");
+    }
+}
