15712b2cee52c639e0cf4352c318a830e000bf4d
김종혁 <jonnykim1o28@gmail.com>
2025-04-10 01:06:50 +0900

    요구사항 1번 HomeController

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class HomeController extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/home.jsp");
+        rd.forward(req, resp);
+    }
+}
