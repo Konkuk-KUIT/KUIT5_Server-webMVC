c620f3512cf7b6b99dff512303649ccc069de4d4
김종혁 <jonnykim1o28@gmail.com>
2025-04-10 03:46:55 +0900

    요구사항 5: 회원 목록 및 개인정보 수정 보안 강화

--- src/main/java/controller/ListUserController.java
+++ src/main/java/controller/ListUserController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jwp.model.User;
 
 import java.io.IOException;
@@ -14,12 +15,28 @@ import java.util.Collection;
 
 @WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Collection<User> users = MemoryUserRepository.getInstance().findAll();
-        req.setAttribute("users", users);
 
-        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
-        rd.forward(req, resp);
+        HttpSession session = req.getSession();
+
+        if(session.getAttribute("user") == null) {
+
+            resp.sendRedirect("/user/login.jsp");
+
+        }
+
+        if(session.getAttribute("user") != null) {
+            Object value = session.getAttribute("user");
+            User user = (User) value;
+
+
+            Collection<User> users = MemoryUserRepository.getInstance().findAll();
+            req.setAttribute("users", users);
+            req.setAttribute("userId", user.getUserId());
+            RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
+            rd.forward(req, resp);
+        }
     }
 }
