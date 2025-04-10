741cd390f5aa7a6c48e39668e1a73162767af93b
WooHeon <achldngjs@naver.com>
2025-04-10 15:02:58 +0900

    요구사항 5 : 회원 목록 및 개인정보 수정 보안 강화

--- src/main/java/controller/ListUserController.java
+++ src/main/java/controller/ListUserController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jwp.model.User;
 
 import java.io.IOException;
@@ -19,12 +20,22 @@ public class ListUserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+
+        if(value == null){
+            RequestDispatcher rd = req.getRequestDispatcher("/user/login.jsp");
+            rd.forward(req, resp);
+            return;
+        }
+
+        if (value != null) {
+            User user = (User) value;
+        }
+
         Collection<User> users = MemoryUserRepository.getInstance().findAll();
-
         req.setAttribute("users", users);
-
         RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
-
         rd.forward(req, resp);
 
     }
--- src/main/java/controller/UpdateUserFormController.java
+++ src/main/java/controller/UpdateUserFormController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jwp.model.User;
 
 import java.io.IOException;
@@ -17,10 +18,32 @@ public class UpdateUserFormController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String userId = req.getParameter("userId");
-        User user = MemoryUserRepository.getInstance().findUserById(userId);
 
-        req.setAttribute("user", user);
+        HttpSession session = req.getSession();
+        User sessionUser = null;
+        Object value = session.getAttribute("user");
+
+        if(value == null){
+            RequestDispatcher rd = req.getRequestDispatcher("/user/login.jsp");
+            rd.forward(req, resp);
+            return;
+        }
+
+        if (value != null) {
+            sessionUser = (User) value;
+        }
+
+        String editUserId = req.getParameter("userId");
+        User editUser = MemoryUserRepository.getInstance().findUserById(editUserId);
+
+        if(!sessionUser.getUserId().equals(editUserId)){
+            resp.sendRedirect("/user/userList");
+            return;
+        }
+
+
+        req.setAttribute("user", editUser);
+
 
         RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
         rd.forward(req, resp);
